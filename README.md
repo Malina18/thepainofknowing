23b19ec0c7ed630d8a9f71fec030979c720e7ac1
Vivi <vivien.klose@stud-mail.uni-wuerzburg.de>
2023-05-24 17:53:39 +0200

    Simple 2D Movement + Camera

---
+++ README.md
@@ -0,0 +1,27 @@
+# IPSN Team 7 Repository
+
+|  General Info  | |
+| ---|---|
+| Title |  |
+| Students | |
+| Target Platform(s) |   | 
+| Study Program |  | 
+
+### Abstract
+
+__Briefly summarize the project__
+
+### Installation Guide
+
+__How to install__
+
+## Repository Usage 
+
+```
+RepositoryRoot/
+    ├── README.md           
+    ├── binaries/           // .exe 
+    ├── code/
+    └── documentation/      // Project documentation 
+```
+
---
+++ code/The Pain of Knowing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float horizontal;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpingPower;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if(Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0.5f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
